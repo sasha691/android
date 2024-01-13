6f54ca9a174d556e640baeadd58c9bb57fc65a80
sasha691 <komp9349@gmail.com>
2024-01-13 14:35:54 +0200

    cerate class item and fix bag sql

--- app/src/main/java/com/example/mymagazun/DbHelper.kt
+++ app/src/main/java/com/example/mymagazun/DbHelper.kt
@@ -1,11 +1,12 @@
 package com.example.mymagazun
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DbHelper(val context: Context, val factory: SQLiteDatabase.CursorFactory?) :
+class DbHelper(private val context: Context, private val factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, "app", factory, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         val query = "CREATE TABLE users (id INT PRIMARY KEY, login TEXT, email TEXT, password TEXT)"
@@ -13,7 +14,7 @@ class DbHelper(val context: Context, val factory: SQLiteDatabase.CursorFactory?)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db!!.execSQL("DROP TABLE IF EXISTS useres")
+        db!!.execSQL("DROP TABLE IF EXISTS users")
         onCreate(db)
     }
 
@@ -29,10 +30,11 @@ class DbHelper(val context: Context, val factory: SQLiteDatabase.CursorFactory?)
         db.close()
     }
 
+    @SuppressLint("Recycle")
     fun getUser(login: String, password: String): Boolean {
         val db = this.readableDatabase
 
-        val result = db.rawQuery("SELECT * FROM user WHERE login = '$login' AND password = '$password'", null)
+        val result = db.rawQuery("SELECT * FROM users WHERE login = '$login' AND password = '$password'", null)
         return result.moveToFirst()
     }
 }
--- app/src/main/java/com/example/mymagazun/ItemsActivity.kt
+++ app/src/main/java/com/example/mymagazun/ItemsActivity.kt
@@ -2,10 +2,22 @@ package com.example.mymagazun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
+
+        val itemsList: RecyclerView = findViewById(R.id.itemsList)
+        val items = arrayListOf<Item>()
+
+        items.add(Item(1, "sofa", "Диван", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut labore", 999))
+        items.add(Item(2, "light", "Освітлення", "Lorem ipsum sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut labore", 399))
+        items.add(Item(3, "kitchen", "Кухня", "Lorem ipsum sit amet, consectetur adipiscing elit", "Sed do eiusmod tempor incididunt ut labore", 2999))
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = ItemsAdapter(items, this)
     }
 }
